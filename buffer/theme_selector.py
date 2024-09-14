9cad9db4569269550ee01fcc280183773e35b610
Chris Heywood <15127-cheywood@users.noreply.gitlab.gnome.org>
2024-09-14 15:27:58 +1000

    feat: Sync up to current theme menu styling

--- buffer/theme_selector.py
+++ buffer/theme_selector.py
@@ -1,4 +1,4 @@
-from gi.repository import Gtk
+from gi.repository import Gio, Gtk
 
 import buffer.config_manager as config_manager
 
@@ -18,8 +18,27 @@ class ThemeSelector(Gtk.Box):
             f"changed::{config_manager.STYLE}", lambda _o, _k: self.__populate()
         )
 
+    @Gtk.Template.Callback()
+    def _on_option_selected(self, _widget: Gtk.CheckButton) -> None:
+        name = None
+        if self._follow.get_active():
+            name = "follow"
+        elif self._light.get_active():
+            name = "light"
+        elif self._dark.get_active():
+            name = "dark"
+        # Name can be null when checkbutton is unchecked for radio group. A positive check will
+        # follow.
+        if name and config_manager.get_style() != name:
+            config_manager.set_style(name)
+            app = Gio.Application.get_default()
+            app.apply_style()
+
     def __populate(self) -> None:
         style = config_manager.get_style()
-        self._follow.set_active(style == "follow")
-        self._light.set_active(style == "light")
-        self._dark.set_active(style == "dark")
+        if style == "follow":
+            self._follow.set_active(True)
+        elif style == "light":
+            self._light.set_active(True)
+        else:
+            self._dark.set_active(True)
